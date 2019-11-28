73fa5cf9a5c424d34fd8f13db011666af232ffe0
Delevan <523373965@qq.com>
2019-11-28 13:01:46 +0800

    npm parcel

---
+++ src/button.vue
@@ -0,0 +1,15 @@
+<template>
+    <button class="g-button">你好</button>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style scoped lang="scss">
+    .g-button{
+        font-size:14px;
+        color:red;
+    }
+</style>
