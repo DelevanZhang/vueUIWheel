9178e6bc653f0e62d5e17994c63d86725f8a0f99
Delevan <523373965@qq.com>
2019-11-28 15:33:54 +0800

    填写 icon 和 iconPosition 属性

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,17 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 
 <script>
     export default {
+        props:["icon","iconPosition"]
     }
 </script>
 
@@ -15,14 +23,28 @@
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
+        display: inline-flex; justify-content: center; align-items: center;
+        vertical-align: middle;
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+        .icon{
+            width: 1em; height: 1em;
+        }
+        //默认样式图标在左边
+        > .content { order: 2; }
+        > .icon { order: 1; margin-right: .1em; }
+        //图标在右边
+        &.icon-right {
+            > .content { order: 1; }
+            > .icon { order: 2; margin-right: 0; margin-left: .1em;}
+        }
     }
-    .g-button:hover {
-        border-color: var(--border-color-hover);
-    }
-    .g-button:active {
-        background-color: var(--button-active-bg);
-    }
-    .g-button:focus {
-        outline: none;
-    }
+
 </style>
